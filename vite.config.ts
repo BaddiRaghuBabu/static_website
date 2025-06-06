a398128ce354bcc7a489a42264a252e7eac842dd
Baddi raghubabu <baddiraghuram6@gmail.com>
2025-06-06 05:38:58 +0000

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import tailwindcss from "@tailwindcss/vite"
 
 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/static_web/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
